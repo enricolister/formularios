04a82908310b2dac0c5ae9319534bb871523211f
Enrico Lister <enricolister@hotmail.com>
2018-12-07 20:08:56 +0100

    Ciao Rocco

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent {
+
+  constructor() { }
+  guardar() {
+    console.log("Formulario posteado");
+  }
+}
