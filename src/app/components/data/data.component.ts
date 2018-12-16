ac281b8f784b0bb90b0f502ed65fd1847342355d
Enrico Lister <enricolister@hotmail.com>
2018-12-16 02:11:22 +0100

    woaaa

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -15,7 +15,8 @@ export class DataComponent {
       nombre: "Guido",
       apellido: "Neri"
     },
-    email: "guido.neri@gmail.com"
+    email: "guido.neri@gmail.com",
+    'pasatiempos': ["Correr", "Dormir", "Comer"]
   }
 
   constructor() {
@@ -27,12 +28,29 @@ export class DataComponent {
         ]),
         'apellido': new FormControl('', Validators.required),
       }),
-      'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+      'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required)
+      ])
     })
+    //this.forma.setValue(this.usuario);
+  }
+
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('', Validators.required)
+    )
   }
 
   guardarCambios() {
     console.log(this.forma);
+    this.forma.reset({
+      nombreCompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      email: ''
+    });
   }
 
 }
