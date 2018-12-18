4d8f9c127b922045144c0179a00d5f7fc64b6463
Enrico Lister <enricolister@hotmail.com>
2018-12-18 01:33:44 +0100

    asda

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -26,14 +26,21 @@ export class DataComponent {
           Validators.required,
           Validators.minLength(3)
         ]),
-        'apellido': new FormControl('', Validators.required),
+        'apellido': new FormControl('', [Validators.required, this.noHerrera]),
       }),
       'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
       'pasatiempos': new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      'password1' : new FormControl('', Validators.required),
+      'password2':  new FormControl() //validator noigual will be set with the other method just below
     })
     //this.forma.setValue(this.usuario);
+
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ])
   }
 
   agregarPasatiempo() {
@@ -42,15 +49,36 @@ export class DataComponent {
     )
   }
 
+  noHerrera(control: FormControl): { [s:string]:boolean } { //s:string es el tipo de retorno, no es un arreglo
+    if (control.value.toLowerCase() === 'herrera') {
+      return {
+        noherrera:true
+      }
+    }
+    return null;
+  }
+
+  noIgual(control: FormControl): { [s:string]:boolean } { //s:string es el tipo de retorno, no es un arreglo
+
+    let forma: any = this; //en este metodo this.forma es simplemente this
+  
+    if (control.value !== forma.controls['password1'].value) { //si ponemos directamente this.controls nos da error
+      return {
+       noiguales:true
+      }
+    }
+    return null;
+  }
+
   guardarCambios() {
     console.log(this.forma);
-    this.forma.reset({
+    /* this.forma.reset({
       nombreCompleto: {
         nombre: '',
         apellido: ''
       },
       email: ''
-    });
+    }); */
   }
 
 }
