dcd22673ebc1796340c77b0b69ebde6f8dc8c74b
Enrico Lister <enricolister@hotmail.com>
2019-01-09 00:39:48 +0100

    end of formularios section

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -43,6 +43,16 @@ export class DataComponent {
       Validators.required,
       this.noIgual.bind(this.forma)
     ])
+
+    this.forma.controls['username'].valueChanges
+      .subscribe( data => {
+        console.log(data);
+      })
+
+      this.forma.controls['username'].statusChanges
+      .subscribe( data => {
+        console.log(data);
+      })
   }
 
   agregarPasatiempo() {
